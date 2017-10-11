abfd27db08549e547ac981e6675700b957c4ab30
Alan Covarrubias <aecovarru@gmail.com>
2017-10-10 23:21:43 -0700

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170905020040) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
     t.text     "body"
@@ -21,9 +24,9 @@ ActiveRecord::Schema.define(version: 20170905020040) do
     t.integer  "author_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
   end
 
   create_table "admin_users", force: :cascade do |t|
@@ -39,14 +42,14 @@ ActiveRecord::Schema.define(version: 20170905020040) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "game_dates", force: :cascade do |t|
     t.integer "season_id"
     t.date    "date"
-    t.index ["season_id"], name: "index_game_dates_on_season_id"
+    t.index ["season_id"], name: "index_game_dates_on_season_id", using: :btree
   end
 
   create_table "games", force: :cascade do |t|
@@ -54,16 +57,16 @@ ActiveRecord::Schema.define(version: 20170905020040) do
     t.integer "game_date_id"
     t.integer "away_team_id"
     t.integer "home_team_id"
-    t.index ["away_team_id"], name: "index_games_on_away_team_id"
-    t.index ["game_date_id"], name: "index_games_on_game_date_id"
-    t.index ["home_team_id"], name: "index_games_on_home_team_id"
-    t.index ["season_id"], name: "index_games_on_season_id"
+    t.index ["away_team_id"], name: "index_games_on_away_team_id", using: :btree
+    t.index ["game_date_id"], name: "index_games_on_game_date_id", using: :btree
+    t.index ["home_team_id"], name: "index_games_on_home_team_id", using: :btree
+    t.index ["season_id"], name: "index_games_on_season_id", using: :btree
   end
 
   create_table "periods", force: :cascade do |t|
     t.integer "game_id"
     t.integer "quarter"
-    t.index ["game_id"], name: "index_periods_on_game_id"
+    t.index ["game_id"], name: "index_periods_on_game_id", using: :btree
   end
 
   create_table "players", force: :cascade do |t|
@@ -74,8 +77,8 @@ ActiveRecord::Schema.define(version: 20170905020040) do
     t.string  "idstr"
     t.string  "position"
     t.boolean "starter"
-    t.index ["season_id"], name: "index_players_on_season_id"
-    t.index ["team_id"], name: "index_players_on_team_id"
+    t.index ["season_id"], name: "index_players_on_season_id", using: :btree
+    t.index ["team_id"], name: "index_players_on_team_id", using: :btree
   end
 
   create_table "seasons", force: :cascade do |t|
@@ -106,8 +109,8 @@ ActiveRecord::Schema.define(version: 20170905020040) do
     t.float   "pace",              default: 0.0
     t.float   "ortg",              default: 0.0
     t.float   "drtg",              default: 0.0
-    t.index ["intervalable_type", "intervalable_id"], name: "index_stats_on_intervalable_type_and_intervalable_id"
-    t.index ["statable_type", "statable_id"], name: "index_stats_on_statable_type_and_statable_id"
+    t.index ["intervalable_type", "intervalable_id"], name: "index_stats_on_intervalable_type_and_intervalable_id", using: :btree
+    t.index ["statable_type", "statable_id"], name: "index_stats_on_statable_type_and_statable_id", using: :btree
   end
 
   create_table "teams", force: :cascade do |t|
@@ -116,7 +119,7 @@ ActiveRecord::Schema.define(version: 20170905020040) do
     t.string  "abbr"
     t.string  "abbr2"
     t.string  "country"
-    t.index ["season_id"], name: "index_teams_on_season_id"
+    t.index ["season_id"], name: "index_teams_on_season_id", using: :btree
   end
 
 end
