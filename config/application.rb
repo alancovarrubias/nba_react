febac3a09d436407399c486bd86d16347c65d7ae
Alan Covarrubias <aecovarru@gmail.com>
2017-10-15 18:45:57 -0700

    fix

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,10 @@ module NbaReact
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.app_generators.scaffold_controller = :scaffold_controller
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
 
     config.middleware.use Rack::MethodOverride
     config.middleware.use ActionDispatch::Flash
