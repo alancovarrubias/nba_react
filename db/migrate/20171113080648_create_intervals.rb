c0a56eb71f69a2dd392a5c91c94ef65a03b3a597
Alan Covarrubias <aecovarru@gmail.com>
2017-11-13 00:13:13 -0800

    fix

--- db/migrate/20171113080648_create_intervals.rb
+++ db/migrate/20171113080648_create_intervals.rb
@@ -1,7 +1,7 @@
 class CreateIntervals < ActiveRecord::Migration[5.0]
   def change
     create_table :intervals do |t|
-      t.references :end_date, class_name: "GameDate"
+      t.references :start_date, class_name: "GameDate"
       t.references :end_date, class_name: "GameDate"
       t.integer :games_played
     end
