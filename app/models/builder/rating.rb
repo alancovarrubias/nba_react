1dca8d7d59f9f4348d7a0723cd495517202832e4
Alan Covarrubias <aecovarru@gmail.com>
2018-04-17 21:36:04 -0700

    add

--- app/models/builder/rating.rb
+++ app/models/builder/rating.rb
@@ -2,12 +2,16 @@ module Builder
   module Rating
     extend self
     def run(season)
-      Stat.where("season_id = #{season.id} AND ortg = 0.0 AND drtg = 0.0 AND poss_percent = 0.0").each do |stat|
-        puts "Rating Stat #{stat.id}"
-        ortg = stat.calc_ortg
-        drtg = stat.calc_drtg
-        poss_percent = stat.calc_poss_percent
-        stat.update(ortg: ortg, drtg: drtg, poss_percent: poss_percent)
+      stats = Stat.where("season_id = #{season.id} AND ortg = 0.0 AND drtg = 0.0 AND poss_percent = 0.0").limit(100)
+      while stats.length != 0
+        stats.each do |stat|
+          puts "Rating Stat #{stat.id}"
+          ortg = stat.calc_ortg
+          drtg = stat.calc_drtg
+          poss_percent = stat.calc_poss_percent
+          stat.update(ortg: ortg, drtg: drtg, poss_percent: poss_percent)
+        end
+        stats = Stat.where("season_id = #{season.id} AND ortg = 0.0 AND drtg = 0.0 AND poss_percent = 0.0").limit(100)
       end
     end
   end
