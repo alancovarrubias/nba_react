93cf2155066f6403c3c750a22d3a315d1e62ba8a
Alan Covarrubias <aecovarru@gmail.com>
2018-06-07 22:49:31 -0700

    add

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -4,7 +4,7 @@ import Dropdown from './Dropdown';
 const Navbar = ({ brand, links }) => {
   return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="">{ brand }</a>
+          <a className="navbar-brand" href="/">{ brand }</a>
           <Dropdown title={links.title} links={links.data} />
         </nav>
       );
