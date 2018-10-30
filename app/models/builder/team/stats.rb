611d7e63e6955d55e0676ab9d060796cbf573c03
Alan Covarrubias <aecovarru@gmail.com>
2018-10-29 22:50:07 -0700

    add

--- app/models/builder/team/stats.rb
+++ app/models/builder/team/stats.rb
@@ -14,18 +14,19 @@ module Builder
             if type == "prev"
               prev_games.each do |games_back|
                 player_stats = @game.send("#{type}_#{field}_player_stats", games_back, @period).map(&:stat_container)
-                build_stat(team, player_stats, type, games_back)
+                build_stat(team, player_stats, field, type, games_back)
               end
             else
               player_stats = @game.send("#{type}_#{field}_player_stats", @period).map(&:stat_container)
-              build_stat(team, player_stats, type)
+              build_stat(team, player_stats, field, type)
             end
           end
         end
       end
 
-      def build_stat(team, player_stats, type, games_back=nil)
+      def build_stat(team, player_stats, field, type, games_back=nil)
         return if player_stats.empty?
+        games_back = @game.send("prev_#{field}_games").size if type == "season"
         query_hash = { season: @season, game: @game, model: team, games_back: games_back, season_stat: type == "season", period: @period }
         team_stat = ::Stat.find_or_create_by(query_hash)
         team_stat_hash = player_stats.inject {|memo, el| memo.merge(el) {|key, old, new| old + new}}
